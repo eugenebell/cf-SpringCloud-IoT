819cb48e8d3cbdf145d5a326066f3408ab65a17b
Eugene Bell <eugene_bell@yahoo.com>
2016-03-15 23:45:49 +0000

    added @EnableHystrixDashboard

--- cf-API-Microservice/src/main/java/io/pivotal/sensor/IotAPIApplication.java
+++ cf-API-Microservice/src/main/java/io/pivotal/sensor/IotAPIApplication.java
@@ -3,11 +3,13 @@ package io.pivotal.sensor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class IotAPIApplication {
 
 	
